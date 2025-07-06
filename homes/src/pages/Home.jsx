6da7171386c3402e34e97c3f1fc790e1a59b8083
Ingeburgern <56824264+Spekkhogger@users.noreply.github.com>
2025-07-06 20:48:47 +0200

    add classes to home

--- homes/src/pages/Home.jsx
+++ homes/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
     return (
-      <div>
+      <div className="flex flex-col justify-center content-center">
         <h1>Home Page</h1>
         <p>Welcome to the renovation tracker!</p>
       </div>
